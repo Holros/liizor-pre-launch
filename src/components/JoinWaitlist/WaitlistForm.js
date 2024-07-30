f2da0a819cd4b196c50ef68b40477a12feb059dc
Ajayi Olamide <115148692+Holros@users.noreply.github.com>
2024-07-30 10:05:44 +0100

    Update WaitlistForm.js

--- src/components/JoinWaitlist/WaitlistForm.js
+++ src/components/JoinWaitlist/WaitlistForm.js
@@ -48,7 +48,7 @@ export default function WaitlistForm() {
   };
 
   return (
-    <div className="relative flex flex-col gap-3 md:bottom-5">
+    <div className="relative flex flex-col gap-3 md:bottom-5" id="joinWaitlist">
       <Toaster position="top-right" />
       <p>
         Welcome to Liizor, the next-generation crypto trading platform designed
